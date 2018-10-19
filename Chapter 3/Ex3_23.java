e4433a3f94d51c06f5a34b4607b25c3a31cf54f3
Bryton Lunceford <Luncefo.Bry3310@stu.stech.edu>
2018-10-19 17:44:17 -0600

    maaade 3-23

---
+++ Chapter 3/Ex3_23.java
@@ -0,0 +1,23 @@
+/*
+Bryton Lunceford
+10/19/18
+Point in a Rectangle
+*/
+
+import java.util.Scanner;
+
+public class Ex3_23 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a point with two coordinates: ");
+		double x = input.nextDouble();
+		double y = input.nextDouble();
+
+		boolean withinRectangle = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2 ) ||
+										  (Math.pow(Math.pow(y, 2), 0.5) <= 5.0 / 2);
+
+		System.out.println("Point (" + x + ", " + y + ") is " +
+			((withinRectangle) ? "in " : "not in ") + "the rectangle");
+	}
+}
