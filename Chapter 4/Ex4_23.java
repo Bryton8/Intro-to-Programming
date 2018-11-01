083ab70c081d780b8eb81b1919e749291408defd
404kevnotfound <Luncefo.Bry3310@stu.stech.edu>
2018-11-01 08:19:11 -0600

    Corrected errors and added formulas

--- Chapter 4/Ex4_23.java
+++ Chapter 4/Ex4_23.java
@@ -8,20 +8,30 @@ import java.util.Scanner;
 
 public class Ex4_23 {
 	public static void main(String[] args) {
-		Scanner input = new Scaner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter employee: ");
 		String name = input.nextLine();
 		
-		System.out.print("Enter number of hours worked in the week ");
+		System.out.print("Enter number of hours worked in the week: ");
 		double hours = input.nextDouble();
 		
-		System.out.print("Enter hourly rate ");
+		System.out.print("Enter hourly rate: ");
 		double rate = input.nextDouble();
 		
-		System.out.print("Enter federal tax withholding rate ");
+		System.out.print("Enter federal tax withholding rate: ");
 		double ftax = input.nextDouble();
 		
-		System.out.print("Enter state tax withholding rate ");
+		System.out.print("Enter state tax withholding rate: ");
 		double stax = input.nextDouble();
+		
+		System.out.println("Employee Name: " + name);
+		System.out.println("Hours Worked: " + hours);
+		System.out.println("Pay rate: " + rate);
+		System.out.println("Gross Pay: " + rate * hours);
+		System.out.println("Deductions: ");
+		System.out.printf("Federal Withholding (%.1f%%): $%.2f\n",ftax * 100, ftax * rate * hours);
+		System.out.printf("State Withholding (%.1f%%): $%.2f\n",stax * 100, stax * rate * hours);
+		System.out.printf("Total Deduction: $%.2f\n",(stax + ftax) * rate * hours);
+		System.out.printf("Net Pay: $%.2f\n",(1 - stax - ftax) * rate * hours);
 	}
 }
