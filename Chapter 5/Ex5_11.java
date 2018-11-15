94030d712f25242b53329bc33539a6fc04c3dc15
404kevnotfound <Luncefo.Bry3310@stu.stech.edu>
2018-11-15 08:38:40 -0700

    5*11

---
+++ Chapter 5/Ex5_11.java
@@ -0,0 +1,13 @@
+class Ex5_11 {
+	public static void main(String[] args) {
+		
+		int count = 1;
+		for (int i = 100; i <=200; i++) {
+			
+				if (i % 6 == 0 ^ i % 5 == 0) {
+						System.out.print((count ++ % 10 != 0) ? i + " " : i + " \n ");
+						
+			}
+		}	
+	}
+}
